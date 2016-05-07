d5439e6020b753bfa3107551dc5f7980e2a60c22
Daniel Dizzy <dizzylol4@gmail.com>
2016-05-07 16:46:06 +0900

    created the user and school model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController  
+	before_filter :authenticate_user!, :except => [:reset_password]
+
+	ef my_current_user
+    render json: current_user
+  end
+
+  def update_user
+   @user = User.find(params[:id])
+     if @user.update(user_params)
+       head :no_content
+     else
+       render json: @user.errors, status: :unprocessable_entity
+     end
+  end
+
+  def all_users
+  	@users = User.all.count
+  	render json: @users
+  end
+
+  def reset_password
+    @user = User.find_by_email(params[:user_email])
+    @user.send_reset_password_instructions
+    render json: @user
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :avatar)
+  end
+
+
+end 
