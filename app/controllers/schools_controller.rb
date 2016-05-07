da927f802e5c7c64fbdb0b717b4cd062afb18dd3
Daniel Dizzy <dizzylol4@gmail.com>
2016-05-07 16:52:56 +0900

    Configured the school model

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,21 @@
+class SchoolsController < ApplicationController
+	before_action :set_school
+
+	def update
+    if @school.update(school_params)
+      render json: @school
+    end
+  end
+    
+  private
+
+  def school_params
+    params.require(:school).permit(:name, :thumbnail)
+  end
+
+  def set_school
+    @school = School.find(params[:id])
+  end
+
+
+end
